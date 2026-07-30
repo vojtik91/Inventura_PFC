1a7bb8cf9b8c29f4d3afafdf309df93e13005d69
vojtik91 <vojtik91@gmail.com>
2026-07-31 01:55:44 +0200

    PFC v0.2

---
+++ README.txt
@@ -0,0 +1,8 @@
+INVENTURA PFC – VERZE 0.2
+
+1. Nahrajte všechny soubory z této složky do kořene GitHub repozitáře a nahraďte původní.
+2. Počkejte na nové nasazení GitHub Pages.
+3. V telefonu aplikaci úplně zavřete a znovu otevřete. Případně obnovte stránku.
+
+Obsahuje 120 skladových karet z exportu PFC Bar.
+Kategorie jsou prozatím použity jako pořadí/místa. Skutečné pořadí podle provozovny nastavíme v další iteraci.
--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE = "storyous-inventura-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-});
+const C='pfc-inventura-v02';const A=['./','./index.html','./styles.css','./app.js','./cards.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
